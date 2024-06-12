e2e360fdc4ec3381af8e447519ead086fbe6535f
Sastri <vavilala-hanumantha.sastri@capgemini.com>
2024-06-12 12:51:12 +0530

    first commit

---
+++ src/gitProject/Welcome.java
@@ -0,0 +1,9 @@
+package gitProject;
+
+public class Welcome {
+	
+	public static void main(String[] args) {
+		System.out.println("welcome to git");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author vsastri
+ *
+ */
+module GitProject {
+}
